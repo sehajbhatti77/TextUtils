1521e036ce6251516d10a622fd8b12e91a412322
sehajbhatti77 <sehajdeepkulana@gmail.com>
2023-06-27 20:01:39 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function Navbar(props) {
+    const location=useLocation()
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
@@ -13,10 +15,10 @@ export default function Navbar(props) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link ${location.pathname==="/"?"active":""}`}  aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                            <Link className={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">{props.aboutText}</Link>
                         </li>
                     </ul>
                     <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
